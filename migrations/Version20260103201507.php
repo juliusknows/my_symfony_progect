c4eccbd7f953955f3f8b280d824d326dc4bb25d9
juliusknows <ageev_m@icloud.com>
2026-01-04 00:16:47 +0300

    переписал down для миграций

--- migrations/Version20260103201507.php
+++ migrations/Version20260103201507.php
@@ -25,8 +25,10 @@ final class Version20260103201507 extends AbstractMigration
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE SCHEMA public');
-        $this->addSql('DROP INDEX UNIQ_911533C8989D9B62');
+        $conferenceTable = $schema->getTable('conference');
+
+        if ($conferenceTable->hasIndex('uniq_911533c8989d9b62')) {
+            $conferenceTable->dropIndex('uniq_911533c8989d9b62');
+        }
     }
 }
